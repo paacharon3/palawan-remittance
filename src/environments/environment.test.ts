ccd2955945703dab0386d0dda3aa9ba17da6c82a
Pedro Acharon III <pacharon@apollo.com.ph>
2018-01-25 17:24:22 +0800

    [UPDATE]: Added test environment file

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    name: "test",
+    production: true,
+    getRemittanceCentersAPI: 'https://apidemo.cliqq.net:8443/remittance/v1/quick/remcos',
+    authAPI: 'https://apidemo.cliqq.net:8443/accounts/oauth2/token',
+    getServiceFeeAPI: 'https://apidemo.cliqq.net:8443/remittance/v1/quick/fees',
+    sendMoneyAPI: 'https://apidemo.cliqq.net:8443/remittance/v1/quick/transfers',
+    maxTransactionAmount: 2500.00,
+    destinationBranch: "PWA"
+    
+  };
+  
