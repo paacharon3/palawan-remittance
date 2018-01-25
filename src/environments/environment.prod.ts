e519d42c599524880ce4d8680385c26660123e43
Pedro Acharon III <pacharon@apollo.com.ph>
2018-01-25 17:02:33 +0800

    [UPDATE]: added new environment variables for testing, force error on sendCashOut API call in dev build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,10 @@
 export const environment = {
+  name: "prod",
   production: true,
   getRemittanceCentersAPI: 'https://gw.cliqq.net:8443/remittance/v1/quick/remcos',
   authAPI: 'https://gw.cliqq.net:8443/accounts/oauth2/token',
   getServiceFeeAPI: 'https://gw.cliqq.net:8443/remittance/v1/quick/fees',
   sendMoneyAPI: 'https://gw.cliqq.net:8443/remittance/v1/quick/transfers',
-  maxTransactionAmount: 2500.00
+  maxTransactionAmount: 2500.00,
+  destinationBranch: "XXK"
 };
